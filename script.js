718bcde8c49a8f22530fba4005b94b262d42e1f5
saksham-accio <125554544+saksham-accio@users.noreply.github.com>
2024-02-28 00:15:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
-// your code here
+	let n1 = 0;
+	let n2 = 1;
+	if(num==0) return n1;
+	if(num==1) return n2;
+	
+	let n3;
+	for(let i=2; i<num; i++){
+		n3 = n1+n2;
+		n1 = n2;
+		n2 = n3;
+	}
+	return n3;
 }
 
 module.exports = fibonacci;
